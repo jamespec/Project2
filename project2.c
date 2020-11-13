34a04fedc07b987f562f3dd7e3b4e748300c5365
James <james315@icloud.com>
2020-11-13 12:03:34 -0500

    Fixing debugger

--- project2.c
+++ project2.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "parse.h"
 #include "book_list.h"
 
 int findLine(char s[], int lim);
+int iswhitespace( char s[] );
 
 int main() {
     struct book *list_head=0;
@@ -19,10 +21,14 @@ int main() {
 
     int inputLines = 0;
     int accepted=0;
+    int rejected=0;
 
     int count = findLine(line, 300);
     while( count > 0 ) { 
         inputLines++;
+
+        if( iswhitespace(line) == 0 )
+            continue;
 
         int nextChar = getISBN( line, 0, isbn );
         if( nextChar > 0 ) {
@@ -32,25 +38,36 @@ int main() {
                 if( nextChar > 0 ) {
                     nextChar = getFirst( line, nextChar, first );
                     if( nextChar > 0 ) {
-                        accepted++;
-
                         fillBook( &book, isbn, title, last, first );
 
                         struct book *found = findBookByISBN( list_head, isbn );
                         if( found ) {
-                            if( compareBooks( found, &book ) == 0 )
+                            if( compareBooks( found, &book ) == 0 ) {
                                 found->count++;
-                            // else
-                            // mismatched info! Figure out what to do
+                                accepted++;
+                            }
+                            else
+                            {
+                                rejected++;
+                            }
                         }
                         else {
                             // not found, add new
                             list_head = add( list_head, &book );
+                            accepted++;
                         }
                     }
+                    else
+                        rejected++;
                 }
+                else
+                    rejected++;
             }
+            else
+                rejected++;
         }
+        else
+            rejected++;
 
         count = findLine(line, 300);
         nextChar=0;
@@ -59,7 +76,7 @@ int main() {
     printf("%d lines of input wew processed.\n\n", inputLines);
     printBookTitles(list_head);
     printf("\n%d lines of input were accepted.\n", accepted );
-    printf("%d lines of input were rejected.\n", (inputLines-accepted));
+    printf("%d lines of input were rejected.\n", rejected );
 }
 
 //Reference to Get Line Function from textbook
@@ -74,4 +91,14 @@ int findLine(char s[], int lim) {
     }
     s[i] = '\0';
     return i;
+}
+
+int iswhitespace( char s[] ) {
+    int count=0;
+
+    for( int i=0; s[i]; i++ )
+        if( isspace(s[i]) )
+            count++;
+
+    return count;
 }
